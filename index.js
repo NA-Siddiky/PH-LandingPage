adcd77658acfb0c5ccd5401a93036033e190a668
NA-Siddiky <siddiky.academic@gmail.com>
2022-10-17 03:04:34 +0600

    Reviews carousel responsive fixed

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-let items = document.querySelectorAll('.carousel-item-premium-courses')
+let courseItems = document.querySelectorAll('.carousel-item-premium-courses')
 
-items.forEach((el) => {
+courseItems.forEach((el) => {
     const minPerSlide = 4
     let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
+    for (let i=1; i<minPerSlide; i++) {
         if (!next) {
             // wrap carousel by using first child
-        	next = items[0]
+        	next = courseItems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let reviewsItems = document.querySelectorAll('.carousel-item-reviews')
+
+reviewsItems.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    for (let i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = reviewsItems[0]
       	}
         let cloneChild = next.cloneNode(true)
         el.appendChild(cloneChild.children[0])
